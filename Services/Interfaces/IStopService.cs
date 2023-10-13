c17e9ff46c6ecc9c2ff039b5981f36ae626a621a
MitkoMB <mitko.m.barakov@gmail.com>
2023-10-13 21:30:03 +0300

    Started the project

--- Services/Interfaces/IStopService.cs
+++ Services/Interfaces/IStopService.cs
@@ -1,9 +1,11 @@
-﻿using BussesRouteMiniProject.ViewModels;
+﻿using BussesRouteMiniProject.Services.Services.Models;
+using BussesRouteMiniProject.ViewModels;
 
 namespace BussesRouteMiniProject.Services.Interfaces
 {
     public interface IStopService
     {
         public Task<List<StopsByBusIdViewModel>> GetStopsByBusIdAsync(int busId);
+        public Task GetRouteAsync(StopsSearchModel model);
     }
 }
