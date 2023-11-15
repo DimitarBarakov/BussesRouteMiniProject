63b7c4bff4f8c171b160ee0fc996028e3622ec53
MitkoMB <mitko.m.barakov@gmail.com>
2023-11-15 18:08:39 +0300

    Added button to show and hide route's stops

---
+++ wwwroot/js/showStopsInRoute.js
@@ -0,0 +1,18 @@
+﻿let btns = document.querySelectorAll(".showStops")
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", ShowAndHideStops)
+}
+
+function ShowAndHideStops(event) {
+    let parent = event.target.parentElement.parentElement;
+    let info = parent.querySelector('.routeStops')
+    if (event.target.textContent == 'See Stops') {
+        info.style.display = 'block';
+        event.target.textContent = 'Hide Stops'
+    }
+    else {
+        info.style.display = 'none';
+        event.target.textContent = 'See Stops'
+    }
+}
